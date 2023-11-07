d71bb76571268a1e8ce8a16b75f4d1b97f473355
Katia Mendes <katia.mendesx@gmail.com>
2023-11-07 19:49:28 -0300

    atualizacoes

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,18 +1,31 @@
 function Home() {
+
     return (
         <>
-            <div className="w-screen flex justify-center">
-                <div>
-                    <div className="max-w-7xl flex flex-col items-center">
-                        <h2>Seja Bem Vinde!</h2>
-                        <p>Expresse aqui seus pensamentos e opniões</p>
+            <div className="bg-indigo-900 flex justify-center">
+                <div className='container grid grid-cols-2 text-white'>
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className='text-5xl font-bold'>
+                            Seja Bem Vinde!
+                        </h2>
+                        <p className='text-xl'>
+                            Expresse aqui seus pensamentos e opniões
+                        </p>
+
+                        <div className="flex justify-around gap-4">
+                            <div className='rounded text-white 
+                                            border-white border-solid border-2 py-2 px-4'
+                                >
+                                Nova Postagem
+                            </div>
+                        </div>
                     </div>
 
-                    <div className="max-w-7xl flex flex-col items-center">
+                    <div className="flex justify-center ">
                         <img
-                            src="https://i.imgur.com/VpwApCU.png"
-                            alt="Imagem da Página Home"
-                            width="400px"
+                            src="https://i.pinimg.com/564x/5c/6f/d2/5c6fd28111d73fe275fe56f9882f5d8b.jpg"
+                            alt="Imagem Página Home"
+                            className='w-2/3'
                         />
                     </div>
                 </div>
