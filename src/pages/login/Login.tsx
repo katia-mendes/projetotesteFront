f0116a025d1649dce0c64169641e4274b6c69967
Katia Mendes <katia.mendesx@gmail.com>
2023-11-23 06:06:04 -0300

    atualizando

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,14 +1,13 @@
 import { Link, useNavigate } from 'react-router-dom';
 import './Login.css';
-
 import { AuthContext } from '../../contexts/AuthContext';
 import { ChangeEvent, useContext, useEffect, useState } from 'react';
-import UsuarioLogin from '../../models/UsuarioLogin';
 import { RotatingLines } from 'react-loader-spinner';
+import UsuarioLogin from '../../models/UsuarioLogin';
 
 function Login() {
 
-    const navigate = useNavigate();
+    const navigate = useNavigate()
     const { usuario, handleLogin, isLoading } = useContext(AuthContext);
 
     const [usuarioLogin, setUsuarioLogin] = useState<UsuarioLogin>(
@@ -35,61 +34,61 @@ function Login() {
 
     return (
         <>
-            <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold">
-                <form className="flex justify-center items-center flex-col w-1/2 gap-4"
-                    onSubmit={login}>
-                    <h2 className="text-violet-950 text-5xl ">Entrar</h2>
-                    <div className="flex flex-col w-full">
+            <div className='grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold'>
+                <form className='flex justify-center items-center flex-col w-1/2 gap-4' onSubmit = {login}>
+                    <h2 className='text-slate-900 text-5xl'>
+                        Entrar
+                    </h2>
+
+                    <div className='flex flex-col w-full'>
                         <label htmlFor="usuario">Usuário</label>
                         <input
                             type="text"
-                            id="usuario"
-                            name="usuario"
-                            placeholder="usuario"
-                            className="border-2 border-violet-400 rounded p-2"
+                            id='usuario'
+                            name='usuario'
+                            placeholder='Usuario'
+                            className='border-2 border-slate-700 rounded p-2'
                             value={usuarioLogin.usuario}
                             onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
                         />
                     </div>
-                    <div className="flex flex-col w-full">
-                        <label htmlFor="senha" className="text-violet-900">Senha</label>
+
+                    <div className='flex flex-col w-full'>
+                        <label htmlFor="senha">Senha</label>
                         <input
                             type="password"
-                            id="senha"
-                            name="senha"
-                            placeholder="Senha"
-                            className="border-2 border-violet-400 rounded p-2"
+                            id='senha'
+                            name='senha'
+                            placeholder='Senha'
+                            className='border-2 border-slate-700 rounded p-2'
                             value={usuarioLogin.senha}
                             onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
                         />
                     </div>
-                    <button
-                        type='submit'
-                        className="rounded bg-violet-500 flex justify-center
-                                   hover:bg-violet-800 text-white w-1/2 py-2">
+
+                    <button type='submit' className='rounded bg-indigo-400 flex justify-center hover:bg-indigo-900 text-white w-1/2 py-2'>
                         {isLoading ? <RotatingLines
-                            strokeColor="white"
-                            strokeWidth="5"
-                            animationDuration="0.75"
-                            width="24"
+                            strokeColor='white'
+                            strokeWidth='5'
+                            animationDuration='0.75'
+                            width='24'
                             visible={true}
                         /> :
-                            <span>Entrar</span>}
+                            <span>Entrar</span>
+                        }
                     </button>
 
-                    <hr className="border-slate-800 w-full" />
+                    <hr className='border-slate-800 w-full' />
 
-                    <p className="flex">
-                        <p className="px-1 text-violet-950">Não tem uma conta?{' '}</p>
-                        <Link to="/cadastro" className="text-violet-500 hover:no-underline">
-                            Cadastre-se
-                        </Link>
+                    <p>
+                        Ainda não tem uma conta?{' '}
+                        <Link to="/cadastro" className='text-indigo-800 hover:underline' >Cadastre-se</Link>
                     </p>
                 </form>
-                <div className="fundoLogin hidden lg:block"></div>
+                <div className='fundoLogin hidden lg:block'></div>
             </div>
         </>
-    );
+    )
 }
 
-export default Login;
+export default Login
